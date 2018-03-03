2e60013ff5c15b5e1b542843239f5bfbb3111379
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-03-03 23:56:57 +0100

    Added actions.js

---
+++ actions.js
@@ -0,0 +1,45 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMEN = 'THUMB_DOWN_COMMEN';
+
+
+function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    text,
+    id: uuid.v4()
+  }
+}
+
+function editComment(id, text) {
+  return {
+    type: EDIT_COMMENT,
+    text,
+    id
+  }
+}
+
+function removeComment(id) {
+  return {
+    type: REMOVE_COMMENT,
+    id
+  }
+}
+
+function thumbUpComment(id) {
+  return {
+    type: THUMB_UP_COMMENT,
+    id
+  }
+}
+
+function thumbDownComment(id) {
+  return {
+    type: THUMB_DOWN_COMMEN,
+    id
+  }
+}
